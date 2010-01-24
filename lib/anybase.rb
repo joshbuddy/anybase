effa24ba761ad32776ffb44d5b07790d2617fa23
Joshua Hull <joshbuddy@gmail.com>
2010-01-23 23:20:03 -0500

    better specs. better readme. fixed ignore_case stupidity

--- lib/anybase.rb
+++ lib/anybase.rb
@@ -1,12 +1,12 @@
 class Anybase
 
-  attr_reader :chars
+  attr_reader :chars, :char_map, :num_map
   
   UnrecognizedCharacterError = Class.new(RuntimeError)
   
   def initialize(chars, opts = nil)
     @chars = chars
-    @ignore_case = opts && opts[:ignore_case] || false
+    @ignore_case = opts && opts.key?(:ignore_case) ? opts[:ignore_case] : false
     @char_map = Hash.new{|h,k| raise UnrecognizedCharacterError.new("the character `#{k.chr}' is not included in #{@chars}")}
     @num_map = {}
     @chars.split('').each_with_index do |c, i|
@@ -23,7 +23,7 @@ class Anybase
   def to_i(val)
     num = 0
     (0...val.size).each{|i| 
-      num += (chars.size ** (val.size - i - 1)) * @char_map[val[i]]
+      num += (chars.size ** (val.size - i - 1)) * char_map[val[i]]
     }
     num
   end
@@ -33,17 +33,18 @@ class Anybase
     until val.zero?
       digit = val % chars.size
       val /= chars.size
-      str[0, 0] = @num_map[digit]
+      str[0, 0] = num_map[digit]
     end
     if options && options[:zero_pad]
-      str[0, 0] = @num_map[0] * (options[:zero_pad] - str.size)
+      str[0, 0] = num_map[0] * (options[:zero_pad] - str.size)
     end
-    str == '' ? @num_map[0].dup : str
+    str == '' ? num_map[0].dup : str
   end
   
   Hex          = Anybase.new('0123456789abcdef', :ignore_case => true)
+  Base62       = Anybase.new('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')
   Base64       = Anybase.new('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/')
   Base64ForURL = Anybase.new('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_')
-  Base62       = Anybase.new('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')
+  Base66ForURL = Anybase.new('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.~')
   
 end
--- spec/from_spec.rb
+++ spec/from_spec.rb
@@ -10,4 +10,8 @@ describe Anybase, "from" do
     proc { Anybase.new("012345678").to_i('a2350') }.should raise_error(Anybase::UnrecognizedCharacterError)
   end
 
+  it "should fold case" do
+    Anybase.new("012345678abcd", :ignore_case => true).to_i('a23D5d0AbBc').should == Anybase.new("012345678abcd", :ignore_case => true).to_i('A23d5D0aBbC')
+  end
+
 end
